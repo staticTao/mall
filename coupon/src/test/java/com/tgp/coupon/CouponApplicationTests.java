8c85e3aa14f675b2b19591269a10e1266e1c0e82
TGP <1227470298@qq.com>
2022-08-27 12:27:44 +0800

    创建项目模块

---
+++ coupon/src/test/java/com/tgp/coupon/CouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgp.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ member/src/test/java/com/tgp/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgp.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
